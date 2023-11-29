591bebdcacae1914f9319377d9bea8a4ca20ac50
Dahee Park <dapark@cx2r7s1.42seoul.kr>
2023-11-29 16:30:37 +0900

    add header

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -1,6 +1,16 @@
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
 
+#include "Webserv.hpp"
 
+class Request
+{
+	private:
+		std::string method;
+		std::string location;
+		std::string version;
+		std::map<std::string, std::string> headers;
+		std::string body;
+};
 
 #endif
--- includes/Response.hpp
+++ includes/Response.hpp
@@ -1,6 +1,16 @@
 #ifndef RESPONSE_HPP
 #define RESPONSE_HPP
 
+#include "Webserv.hpp"
 
+class Response
+{
+	private:
+		std::string version;
+        int         status;
+        std::string status_msg;
+		std::map<std::string, std::string> headers;
+		std::string body;
+};
 
 #endif
