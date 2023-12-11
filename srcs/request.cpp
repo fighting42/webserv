9d26942f484c19b34f117664ac01e44eb9b4d067
daheeeing <sharon1221@naver.com>
2023-12-11 17:25:36 +0900

    <request parsing> done

--- srcs/request.cpp
+++ srcs/request.cpp
@@ -1,7 +1,7 @@
 #include "../includes/Request.hpp"
 
 Request::Request()
-    :method("default"), location("default"), version("default"), body("default"), headers("defaut", "default")
+    :method("default"), location("default"), version("default"), body("default")
 {}
 
 Request::~Request(){}
@@ -18,18 +18,22 @@ std::vector<std::string> Request::ReqSplit(std::string input, char delimiter)
     if (delimiter == ':')
     {
         if (ret.size() == 1)
-            return static_cast< std::vector<std::string> > (NULL);
+            return (static_cast< std::vector<std::string> > (0));
     }
     return ret;
 }
 
 std::string Request::removeSpace(std::string str)
 {
-    
+    std::string tmp;
+
+    tmp = str.substr(1, str.size());
+    return (tmp);
 }
 
-void Request::ReqParsing()
+void Request::ReqParsing(std::string msg)
 {
+    this->req_msg = msg;
     std::size_t found = this->req_msg.find("\n");
     std::string first = this->req_msg.substr(0, found);
     std::vector<std::string> firstline;
@@ -44,15 +48,36 @@ void Request::ReqParsing()
     std::vector<std::string> vline;
     while(1)
     {
-        found_tmp = this->req_msg.find("\n", found);
-        line = this->req_msg.substr(found+1, found_tmp);
+        if (found >= this->req_msg.size())
+            break;
+        found_tmp = this->req_msg.find("\n", found+1);
+        line = this->req_msg.substr(found+1, found_tmp - (found + 1));
         vline = ReqSplit(line, ':');
-        if (vline == static_cast< std::vector<std::string> > (NULL))
+        if (vline == static_cast< std::vector<std::string> > (0))
             break ;
         vline[1] = removeSpace(vline[1]);
-        this->headers.insert(vline[0], vline[1]);
+        this->headers.insert(std::make_pair(vline[0], vline[1]));
         found = found_tmp;
     }
-    if (found != this->req_msg.size())
+    if (found != this->req_msg.size() && found < this->req_msg.size())
         this->body = this->req_msg.substr(found, this->req_msg.size());
+    // std::cout << this->method << std::endl;
+    // std::cout << this->location << std::endl;
+    // std::cout << this->version << std::endl;
+    // std::map<std::string, std::string>::iterator it;
+    // for (it = this->headers.begin(); it != this->headers.end(); it++) {
+    //     std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+    // }
+    // std::cout << this->body << std::endl;
+
+}
+
+
+int main()
+{
+    Request Re;
+    std::string msg;
+
+    msg = "GET http://developer.mozilla.org/ko/docs/Web/HTTP/Message HTTP/1.1\nasdfadsfasdf";
+    Re.ReqParsing(msg);
 }
