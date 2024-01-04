f87595bfb9d9ff134c7f8a913c4e984948ba1add
daheeeing <sharon1221@naver.com>
2024-01-04 16:48:30 +0900

    fix the Request

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -12,21 +12,22 @@ class Request
 	private:
     std::string req_msg;
 		std::string method;
-		std::string location; // uri로 변수명 변경..해주세요
+		std::string uri;
 		std::string version;
-    // std::string host; // headers 중에 Host만.. 변수로 빼주세요
+    std::string host;
 		std::map<std::string, std::string> headers;
 		std::string body;
     
   public:
     Request();
     ~Request();
+    const std::string& getMethod() const;
+	  const std::string& getUri() const;
+	  const std::string& getHost() const;
+    void PrintRequest();
     void ReqParsing(std::string msg);
     std::vector<std::string> ReqSplit(std::string input, char delimiter);
     std::string removeSpace(std::string str);
-    void PrintRequest();
-
-    // 변수들 getter 만들어주세요..ㅜ
 };
 
 #endif
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,10 +1,29 @@
 #include "../includes/Request.hpp"
 
 Request::Request()
-    :method("default"), location("default"), version("default"), body("default")
+    :method("default"), uri("default"), version("default"), body("default")
 {}
 
+const std::string &Request::getMethod() const { return this->method; }
+const std::string &Request::getUri() const { return this->uri; }
+const std::string &Request::getHost() const { return this->host; }
+
 Request::~Request(){}
+
+void Request::PrintRequest()
+{
+    std::cout << "\033[1;94m" << std::endl; // blue
+    std::cout << this->method << std::endl;
+    std::cout << this->uri << std::endl;
+    std::cout << this->version << std::endl;
+    std::map<std::string, std::string>::iterator it;
+    for (it = this->headers.begin(); it != this->headers.end(); it++) {
+        if (it->first == "Content-Length")
+            std::cout << "Content-Length: " << it->second << std::endl;
+        break;
+    }
+    std::cout << "\033[0m" << std::endl; // reset
+}
 
 std::vector<std::string> Request::ReqSplit(std::string input, char delimiter) 
 {
@@ -12,13 +31,19 @@ std::vector<std::string> Request::ReqSplit(std::string input, char delimiter)
     std::stringstream ss(input);
     std::string temp;
  
-    while (getline(ss, temp, delimiter)) {
-        ret.push_back(temp);
-    }
     if (delimiter == ':') //헤더에서 localhost:8080 같은거 들어올 때 처리하기
     {
+        getline(ss, temp, delimiter);
+        ret.push_back(temp);
         if (ret.size() == 1)
             return (static_cast< std::vector<std::string> > (0));
+        return ret;
+    }
+    else
+    {
+        while (getline(ss, temp, delimiter)) {
+            ret.push_back(temp);
+        }
     }
     return ret;
 }
@@ -31,20 +56,6 @@ std::string Request::removeSpace(std::string str)
     return (tmp);
 }
 
-void Request::PrintRequest()
-{
-    std::cout << "\033[1;94m" << std::endl; // blue
-    std::cout << this->method << std::endl;
-    std::cout << this->location << std::endl;
-    std::cout << this->version << std::endl;
-    std::map<std::string, std::string>::iterator it;
-    for (it = this->headers.begin(); it != this->headers.end(); it++) {
-        if (it->first == "Content-Length")
-            std::cout << "Content-Length: " << it->second << std::endl;
-        break;
-    }
-    std::cout << "\033[0m" << std::endl; // reset
-}
 
 void Request::ReqParsing(std::string msg)
 {
@@ -55,9 +66,7 @@ void Request::ReqParsing(std::string msg)
 
     firstline = ReqSplit(first, ' ');
     this->method = firstline[0];
-    if (this->method != "GET" || this->method != "POST" || this->method != "DELETE")
-        return ;
-    this->location = firstline[1];
+    this->uri = firstline[1];
     this->version = firstline[2];
 
     std::size_t found_tmp;
@@ -74,6 +83,8 @@ void Request::ReqParsing(std::string msg)
             break ;
         vline[1] = removeSpace(vline[1]);
         this->headers.insert(std::make_pair(vline[0], vline[1]));
+        if (vline[0] == "Host")
+            this->host = vline[1];
         found = found_tmp;
     }
     if (found != this->req_msg.size() && found < this->req_msg.size())
