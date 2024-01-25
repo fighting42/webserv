18a2cb395e434d419b3a3c9dce209de54a641e00
daheeeing <sharon1221@naver.com>
2024-01-25 10:29:49 +0900

    normal chunked complete

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,8 +1,11 @@
 #include "../includes/Request.hpp"
 
 Request::Request()
-    :method("default"), uri("default"), version("default"), status("200"), query_str(""), chunked(false)
-{}
+    :method("default"), uri("default"), version("default"), status("200"), query_str(""), chunked(false), chunked_done(false), body_done(false)
+{
+    this->body.clear();
+    this->buffer.clear();
+}
 
 const std::string &Request::getMethod() const { return this->method; }
 const std::string &Request::getUri() const { return this->uri; }
@@ -39,36 +42,78 @@ std::vector<std::string> Request::ReqSplit(std::string input, char delimiter)
     return ret;
 }
 
-void Request::controlChunked(size_t found, int flag)
+void Request::controlChunked(size_t found)
 {
-    std::map<std::string, std::string>::iterator it;
-    ssize_t length;
-    for (it = this->headers.begin(); it != this->headers.end(); it++) {
-        if (it->first == "Content-Length")
-            length = hexToDec(it->second);
-        break;
-    }
-    if (found != this->req_msg.size() && found < this->req_msg.size()) {
-            std::string buf = this->req_msg.substr(found, this->req_msg.size());
-            for (size_t i = 0; i < buf.size() ; i++)
-                this->buffer.push_back(buf[i]);
-    }
-    if (this->buffer.size() != static_cast<size_t>(length) || length == -1) {
-        this->status = "404"; //에러 코드 맞는지 확인하기 -> 사이즈 확인
-        return ;
-    }
-    if (flag == 0) //첫번째 chunked일 때
-    {
-        for (size_t i = 0; i < this->buffer.size() ; i++) {
-                this->body.push_back(this->buffer[i]);
-                this->body_size++;
+    ssize_t size, chk_size;
+    std::size_t  found_RN, found_C, i=0;
+    std::string line_size, line_contents;
+    std::vector<char> tmp;
+
+    if (this->req_msg.find("0\r\n\r\n", found+1) != std::string::npos) //이후 확인 필요
+        this->chunked_done = true;
+    found_RN = this->req_msg.find("\r\n", found+1);
+    line_size = this->req_msg.substr(found+1, found_RN-(found+1));
+    size = hexToDec(line_size);
+    while(!this->body_done) {
+        chk_size = 0;
+        tmp.clear();
+        if (found >= this->req_msg.size())
+            break;
+        if (i > 0) {
+            found_RN = this->req_msg.find("\r\n", found+2);
+            line_size = this->req_msg.substr(found+2, found_RN-(found+2));
+            size = hexToDec(line_size);
+            if (this->req_msg.find("\r\n\r\n") == found_RN)
+                this->body_done = true;
         }
+        // std::cout << "chunked size : " << size << std::endl; //삭제
+        while(chk_size < size) {
+            found_C = this->req_msg.find("\r\n", found_RN + 2);
+            line_contents = this->req_msg.substr(found_RN + 2, found_C-(found_RN+2));
+            chk_size += (line_contents.size());
+            // std::cout << "chk_size: " << chk_size << "line_size " << line_contents.size() << std::endl; //삭제
+            if (chk_size + 2 < size) {
+                for (size_t i = 0; i < line_contents.size() ; i++)
+                    tmp.push_back(line_contents[i]);
+                tmp.push_back('\r');
+                tmp.push_back('\n');
+                // for (std::vector<char>::iterator it = tmp.begin(); it != tmp.end() ; ++it)
+                //     std::cout << *it;
+                // std::cout << '\n';
+                chk_size += 2;
+            }
+            else  if (chk_size == size) {
+                // std::cout << "same" << std::endl;
+                for (size_t i = 0; i < line_contents.size() ; i++)
+                    tmp.push_back(line_contents[i]);
+                // for (std::vector<char>::iterator it = tmp.begin(); it != tmp.end() ; ++it)
+                //     std::cout << *it;
+                // std::cout << '\n';
+                break;
+            }
+            else 
+                break;
+            found_RN = found_C;
+        }
+        // std::cout << "tmp size " << tmp.size() << std::endl; //삭제
+        if (tmp.size() != static_cast<size_t>(size) || size == -1) { //size맞지 않을 때
+            this->buffer.clear();
+            // std::cout << "error" <<std::endl; //삭제
+            this->status = "400"; //에러 코드 맞는지 확인하기 -> chat gpt피셜 보통 400이나 500이 나온다고
+            return ;
+        }
+        for (size_t i = 0; i < tmp.size() ; i++)
+            this->buffer.push_back(tmp[i]);
+        found = found_C;
+        i++;
     }
-    else // 첫번째 chunked가 아닐 때
+    if (this->chunked_done)
     {
-
+        this->chunked = false;
+        for (size_t i = 0; i < this->buffer.size() ; i++)
+                this->body.push_back(this->buffer[i]);
+        this->body_size = this->body.size();
     }
-
     return ;
 }
 
@@ -100,7 +145,6 @@ std::size_t Request::LineParsing(std::string msg)
             break ;
         vline[1] = removeWhiteSpace(vline[1]);
         this->headers.insert(std::make_pair(vline[0], vline[1]));
-        // std::cout << "헤더 종류(" << vline[0] << "), 헤더 내용(" << vline[1] << ")" << std::endl;
         if (vline[0] == "Host")
             this->host = vline[1];
         if (vline[0] == "Content-Length")
@@ -112,21 +156,17 @@ std::size_t Request::LineParsing(std::string msg)
     return (found);
 }
 
-//"0/n" chunked라면 content-length(16진법)가 지금 들어온 바디 size 체크 용으로만 사용하고 마지막 문자 나올 떄까지 받아
-
 void Request::ReqParsing(std::string msg)
 {
     std::size_t found;
-    if (this->chunked) //chunked인데 첫번째 아닐 떄
-    {
-        found = LineParsing(msg);
-        controlChunked(found, 1);
-        return ;
-    }
+
     found = LineParsing(msg);
-    if (this->chunked)
-        controlChunked(found, 0); // 첫번째 chunked일 때
-    else {
+    if (this->chunked) {
+        controlChunked(found);
+        if (this->chunked == false)
+            this->chunked_done = false;
+    }
+    else { //chunked아닐 때 body
         if (found != this->req_msg.size() && found < this->req_msg.size()) {
             std::string buf = this->req_msg.substr(found, this->req_msg.size());
             for (size_t i = 0; i < buf.size() ; i++)
@@ -134,13 +174,15 @@ void Request::ReqParsing(std::string msg)
         }
         this->body_size = this->body.size();
     }
+    return ;
 }
 
 // int main() // 파싱 테스트
 // {
 //     Request Req;
 
-//     std::string msg = "POST HTTP?name 1.1\nHost: foo.com\nContent-Type: application/x-www-form-urlencoded\nhost: localhost:8080\nContent-Length: 13\nsay=Hi&to=Mom";
+//     // std::string msg = "POST HTTP?name 1.1\nHost: foo.com\nContent-Type: application/x-www-form-urlencoded\nhost: localhost:8080\nTransfer-Encoding: chunked\n4\r\nWiki\r\n5\r\npedia\r\nE\r\nin\r\n\r\nchunks.\r\n0\r\n\r\n";
+//     std::string msg = "POST HTTP?name 1.1\nHost: foo.com\nContent-Type: application/x-www-form-urlencoded\nhost: localhost:8080\nTransfer-Encoding: chunked\n4\r\nWiki\r\n5\r\npedia\r\n2\r\nin\r\n7\r\nchunks.\r\n0\r\n\r\n";
 //     Req.ReqParsing(msg);
 //     Req.PrintRequest();
 //     return (0);
